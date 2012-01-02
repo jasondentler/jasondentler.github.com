ec7dd7a359b69d45786127dc4f47e9304a216461
Jason Dentler <jasondentler@gmail.com>
2012-01-02 10:31:41 -0600

    Publish with rake at 2012-01-02 10:31:41 -0600

--- rakefile.rb
+++ rakefile.rb
@@ -1,3 +1,7 @@
+require "stringex"
+new_post_ext = "md"
+posts_dir = "_posts"
+
 task :default => [:jekyll]
 task :publish => [:githubpublish, :sitemap]
 
@@ -36,6 +40,27 @@ task :sitemap do
   end
 end
 
+# borrowed this from octopress
+# usage rake new_post[my-new-post] or rake new_post['my new post'] or rake new_post (defaults to "new-post")
+desc "Begin a new post in _posts"
+task :new_post, :title do |t, args|
+  mkdir_p "#{posts_dir}"
+  args.with_defaults(:title => 'new-post')
+  title = args.title
+  filename = "#{posts_dir}/#{Time.now.strftime('%Y-%m-%d')}-#{title.to_url}.#{new_post_ext}"
+  if File.exist?(filename)
+    abort("rake aborted!") if ask("#{filename} already exists. Do you want to overwrite?", ['y', 'n']) == 'n'
+  end
+  puts "Creating new post: #{filename}"
+  open(filename, 'w') do |post|
+    post.puts "---"
+    post.puts "layout: post"
+    post.puts "title: \"#{title.gsub(/&/,'&amp;')}\""
+    post.puts "date: #{Time.now.strftime('%Y-%m-%d %H:%M')}"
+    post.puts "tags: "
+    post.puts "---"
+  end
+end
 
 def clean
 	sh 'rm -rf _site'
